2d46c7bc16efccf78f17eb32829059edbd17dd61
Vokymir <voky.consultant@gmail.com>
2026-04-11 13:38:32 +0200

    clean

--- after/ftdetect/glsl.vim
+++
@@ -1,1 +0,0 @@
-autocmd BufRead,BufNewFile *.vert,*.frag,*.geom,*.tesc,*.tese,*.comp set filetype=glsl
--- lua/custom/own/typst_invoke.lua
+++
@@ -1,5 +0,0 @@
-local typst = require 'custom.own.typst'
-
-vim.keymap.set('n', '<leader>tw', typst.watch, { desc = '[t]ypst [w]atch' })
-vim.keymap.set('n', '<leader>tl', typst.enforce_layout, { desc = '[t]ypst fix [l]ayout' })
-vim.keymap.set('n', '<leader>tp', typst.preview, { desc = '[t]ypst [p]review PDF' })
